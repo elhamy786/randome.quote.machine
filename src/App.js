0b1c56a7df03480529b0d9c8efc64584b301d4af
elhamy786 <elham.afzali1383@gmail.com>
2024-08-30 11:34:53 +0430

    Edit app.js file

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ function App() {
 
   const fetchQuote = async () => {
     try {
+      console.log('Fetching quote...');
       const response = await fetch('https://api.quotable.io/random');
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error(`Network response was not ok: ${response.statusText}`);
       }
       const data = await response.json();
+      console.log('Quote fetched successfully:', data);
       setQuote({ text: data.content, author: data.author });
       setLoading(false);
     } catch (error) {
+      console.error('Error fetching quote:', error);
       setError(error);
       setLoading(false);
     }
@@ -26,6 +29,8 @@ function App() {
   }, []);
 
   const handleNewQuote = () => {
+    setLoading(true);
+    setError(null);
     fetchQuote();
   };
 
@@ -36,9 +41,7 @@ function App() {
   if (error) {
     return (
       <p>
-        Error loading quote:
-        {' '}
-        {error.message}
+        Error loading quote: {error.message}
       </p>
     );
   }
@@ -46,11 +49,7 @@ function App() {
   return (
     <div id="quote-box">
       <p id="text">{quote.text}</p>
-      <p id="author">
-        -
-        {' '}
-        {quote.author}
-      </p>
+      <p id="author">- {quote.author}</p>
       <button
         id="new-quote"
         type="button"
@@ -64,7 +63,7 @@ function App() {
         target="_blank"
         rel="noopener noreferrer"
       >
-        tweet
+        Tweet
       </a>
     </div>
   );
