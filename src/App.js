14d904feec911c4afec2c231dc4ce85d47ed7a5e
elhamy786 <elham.afzali1383@gmail.com>
2024-09-14 10:59:16 +0430

    Resolve errors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const fetchQuote = async () => {
     try {
-      const response = await fetch('https://zenquotes.io/api/random');
+      const response = await fetch('https://cors-anywhere.herokuapp.com/https://zenquotes.io/api/random');
 
       if (!response.ok) {
         throw new Error(`Network response was not ok: ${response.statusText}`);
